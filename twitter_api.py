6b51f2f4a16f2b56ffa2f16925e175c1f931aefc
Waterfox <waterfox80@gmail.com>
2021-08-27 13:18:38 +0200

    Remove content

--- twitter_api.py
+++ twitter_api.py
@@ -1,9 +1,9 @@
 import tweepy as tw
 
-consumer_key= 'U2rLmrQqRWZxWk1tIUmVGTzVQ'
-consumer_secret= '6oJK35VUYddavMKjywwISknny36DqtDLx7Z01iykNpI6ZjYf7c'
-access_token= '1180935927885156353-BfsGe1UdA1hDuNWmSVCdZMjmRaGixt'
-access_token_secret= 'mpr7ZNz691X0x9P3sp4sdkjPJXneHzvanTYV1UpCQDLWB'
+consumer_key= ''
+consumer_secret= ''
+access_token= ''
+access_token_secret= ''
 
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
