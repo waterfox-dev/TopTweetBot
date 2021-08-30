cf371cbb81dda86a6a146a6e14a4d283d9dcc2d8
Waterfox <waterfox80@gmail.com>
2021-08-30 12:26:17 +0200

    working on javascript scripts

---
+++ source/static/scripts/settings.js
@@ -0,0 +1,5 @@
+var settingsPopup   = document.getElementById("settingsPopup");
+var settingsButton  = document.getElementById("buttonSettings");
+var settingsContent = document.getElementById("settingsContent");
+
+settingsButton.on
