3c7571aadb45f66187fde59d01cb69878532409b
Clément Baratin <bobbot.contact@gmail.com>
2021-08-30 16:34:26 +0200

    Adding popup scripts

--- source/static/scripts/settings.js
+++ source/static/scripts/settings.js
@@ -2,4 +2,12 @@ var settingsPopup   = document.getElementById("settingsPopup");
 var settingsButton  = document.getElementById("buttonSettings");
 var settingsContent = document.getElementById("settingsContent");
 
-settingsButton.on
+settingsButton.onclick = function() {
+    settingsPopup.style.display = "block";
+}
+
+window.onclick = function(event) {
+    if (event.target == settingsPopup){
+        settingsPopup.style.display = "none";
+    }
+}
